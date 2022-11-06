8aa917f5157424b69ccc49d2a0b8353e6f2a8de0
Master <blinkerion@gmail.com>
2022-11-06 14:28:21 -0600

    fail attempt come back later

---
+++ my-app/src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SpreadsheetFile from "./SpreadsheetFile";
+
+function Header() {
+    let spreadsheetFile = SpreadsheetFile()
+    
+    return 
+}
+
+export default Header
---
+++ my-app/src/SpreadsheetFile.jsx
@@ -0,0 +1,16 @@
+function SpreadsheetFile() {
+    return [
+        {
+          "id": 1,
+          "name": "Richard P",
+          "email": "blinkerion@gmail.com"
+        },
+        {
+          "id": 2,  
+          "name": "Dafni B",
+          "email": "dafnibatkatov@gmail.com"
+        }
+       ]
+}
+
+export default SpreadsheetFile
